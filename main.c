ea9db706ab92476aa6c04ee9cd84d574ace25b2e
Ablavatskii Oleg <37673007+OlegAProject@users.noreply.github.com>
2020-06-20 19:37:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -15,30 +15,30 @@ static adcsample_t adc_buffer[ADC1_NUM_CHANNELS * ADC1_BUF_DEPTH];
 
 
 static const GPTConfig gpt4cfg1 = {
-  .frequency =  100000,
-  .callback  =  NULL,
-  .cr2       =  TIM_CR2_MMS_1,  // MMS = 010 = TRGO on Update Event.
-  .dier      =  0U
- };
+   .frequency =  100000,
+   .callback  =  NULL,
+   .cr2       =  TIM_CR2_MMS_1,  // MMS = 010 = TRGO on Update Event.
+   .dier      =  0U
+};
 
 static void adccallback(ADCDriver *adcp, adcsample_t *buffer, size_t n)
 {
-	adcp = adcp; n = n;
+    adcp = adcp; n = n;
     k = buffer[0];
 }
 
 static const ADCConversionGroup adcgrpcfg1 = {
-  .circular     = true,
-  .num_channels = ADC1_NUM_CHANNELS,
-  .end_cb       = adccallback,
-  .error_cb     = 0,
-  .cr1          = 0,
-  .cr2          = ADC_CR2_EXTEN_RISING | ADC_CR2_EXTSEL_SRC(12),
-  .smpr1        = ADC_SMPR1_SMP_AN10(ADC_SAMPLE_144),
-  .smpr2        = 0,
-  .sqr1         = ADC_SQR1_NUM_CH(ADC1_NUM_CHANNELS),
-  .sqr2         = 0,
-  .sqr3         = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN10)
+    .circular     = true,
+   .num_channels = ADC1_NUM_CHANNELS,
+   .end_cb       = adccallback,
+   .error_cb     = 0,
+   .cr1          = 0,
+   .cr2          = ADC_CR2_EXTEN_RISING | ADC_CR2_EXTSEL_SRC(12),
+   .smpr1        = ADC_SMPR1_SMP_AN10(ADC_SAMPLE_144),
+   .smpr2        = 0,
+   .sqr1         = ADC_SQR1_NUM_CH(ADC1_NUM_CHANNELS),
+   .sqr2         = 0,
+   .sqr3         = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN10)
 };
 
 void adc_set_and_start(void)
@@ -74,7 +74,7 @@ void sd_set(void)
 char flag = 0;
 static void extcb( EXTDriver *extp, expchannel_t channel)
 {
-	extp = extp;
+    extp = extp;
     channel = channel;
     flag = 1;
 }
@@ -103,7 +103,7 @@ static const EXTConfig extcfg = {
 
 void ext_set_and_start(void)
 {
-	extStart( &EXTD1, &extcfg );
+    extStart( &EXTD1, &extcfg );
 }
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -111,17 +111,16 @@ void ext_set_and_start(void)
 /////////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
-	chSysInit();
+    chSysInit();
     halInit();
     sd_set();
     adc_set_and_start();
     ext_set_and_start();
     while (true)
     {
-    	if (flag == 1)
-    	{
-    		chprintf( (BaseSequentialStream *)&SD7, " %d \n\r", k/23 , 4 );
-    		flag = 0;
+    	if (flag == 1) {
+    	    chprintf( (BaseSequentialStream *)&SD7, " %d \n\r", k/23 , 4 );
+    	    flag = 0;
     	}
     	chThdSleepMilliseconds(50);
     }
